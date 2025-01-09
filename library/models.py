1006105a1346a1af2a3080993e46a81cec2be927
ErgeshovAsan <asan.ergeshov16@gmail.com>
2025-01-09 13:22:21 +0600

    dz_2.1

--- library/models.py
+++ library/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    GENRE_CHOICES = (
+        ('Фантастика', 'Фантастика'),
+        ('Ужасы', 'Ужасы'),
+        ('Боевик', 'Боевик'),
+    )
+    image = models.ImageField(upload_to='books/', verbose_name='Загрузите фото')
+    title = models.CharField(max_length=100, verbose_name='Напишите название книги')
+    description = models.TextField(verbose_name='Укажите описание', blank=True)
+    price = models.PositiveIntegerField(verbose_name='Укажите цену', default=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    genre_choices = models.CharField(max_length=100, choices=GENRE_CHOICES, verbose_name='Выберите жанр', null=True)
+    email = models.EmailField(verbose_name='Напишите почту')
+    director = models.CharField(max_length=100, verbose_name='Укажите автора', default='Владимиров Иван')
+    trailer = models.URLField(verbose_name='Укажите сылку с YOUTUBE')
+
+    class Meta:
+        verbose_name = 'книга'
+        verbose_name_plural = 'список книг' \
+
+    def __str__(self):
+        return f'{self.title} - {self.price} сом'
+
+
