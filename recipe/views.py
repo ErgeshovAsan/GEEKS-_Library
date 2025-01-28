b8956da8c7ba44c86418b281a1fd6e4b004db062
ErgeshovAsan <asan.ergeshov16@gmail.com>
2025-01-28 23:39:03 +0600

    Контрольная работа1

--- recipe/views.py
+++ recipe/views.py
@@ -17,7 +17,7 @@ class RecipeDetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['ingredients'] = self.object.ingredientmodel_set.all()  # Получаем связанные ингредиенты
+        context['ingredients'] = self.object.ingredientmodel_set.all()
         return context
 
 class RecipeCreateView(generic.CreateView):
